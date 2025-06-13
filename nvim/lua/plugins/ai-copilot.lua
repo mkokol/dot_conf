8a28c252e99d996832eb4168902262a0ce80bb20
Mykhailo Kokolius <mickokolius@gmail.com>
2025-06-13 17:35:25 +0200

    - Replace CopilotChat plugin with CodeCompanion

--- nvim/lua/plugins/ai-copilot.lua
+++ nvim/lua/plugins/ai-copilot.lua
@@ -20,42 +20,70 @@ return {
 	},
 
 	-- https://github.com/CopilotC-Nvim/CopilotChat.nvim
+	-- {
+	-- 	"CopilotC-Nvim/CopilotChat.nvim",
+	-- 	branch = "main",
+	-- 	dependencies = {
+	-- 		{ "zbirenbaum/copilot.lua" },
+	-- 		{ "nvim-lua/plenary.nvim", branch = "master" },
+	-- 		{ "nvim-telescope/telescope.nvim" },
+	-- 	},
+	-- 	build = "make tiktoken",
+	-- 	opts = {
+	-- 		model = "claude-sonnet-4",
+	-- 		window = {
+	-- 			layout = "float",
+	-- 			width = 0.9,
+	-- 			height = 0.8,
+	-- 		},
+	-- 		mappings = {
+	-- 			complete = {
+	-- 				insert = "<Tab>",
+	-- 			},
+	-- 			show_diff = {
+	-- 				full_diff = true,
+	-- 				normal = "<Leader>d",
+	-- 			},
+	-- 			accept_diff = {
+	-- 				normal = "<Leader>s",
+	-- 			},
+	-- 			reset = {
+	-- 				normal = "<Leader>r",
+	-- 			},
+	-- 		},
+	-- 	},
+	-- 	keys = {
+	-- 		{ "<leader>ae", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
+	-- 		{ "<leader>aw", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat - Toggle PopUp" },
+	-- 		{ "<leader>am", "<cmd>CopilotChatModels<cr>", desc = "CopilotChat - Select Models" },
+	-- 		{ "<leader>aa", "<cmd>CopilotChatAgents<cr>", desc = "CopilotChat - Select Agents" },
+	-- 	},
+	-- },
+
+	-- https://github.com/olimorris/codecompanion.nvim
+	-- https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua
 	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		branch = "main",
-		dependencies = {
-			{ "zbirenbaum/copilot.lua" },
-			{ "nvim-lua/plenary.nvim", branch = "master" },
-			{ "nvim-telescope/telescope.nvim" },
-		},
-		build = "make tiktoken",
+		"olimorris/codecompanion.nvim",
 		opts = {
-			window = {
-				layout = "float",
-				width = 0.9,
-				height = 0.8,
+			display = {
+				chat = {
+					window = {
+						layout = "float", -- float|vertical|horizontal|buffer
+						width = 0.8,
+						height = 0.8,
+					},
+				},
 			},
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
 		},
 		keys = {
-			{ "<leader>ae", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
-			{ "<leader>aw", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat - Toggle PopUp" },
-			{ "<leader>am", "<cmd>CopilotChatModels<cr>", desc = "CopilotChat - Select Models" },
-			{ "<leader>aa", "<cmd>CopilotChatAgents<cr>", desc = "CopilotChat - Select Agents" },
-		},
-		mappings = {
-			complete = {
-				insert = "<Tab>",
-			},
-			accept_diff = {
-				normal = "<Leader>s",
-			},
-			reset = {
-				normal = "<Leader>r",
-			},
-			show_diff = {
-				full_diff = true,
-				normal = "<Leader>d",
-			},
+			{ "<leader>aw", "<cmd>CodeCompanionChat<cr>", desc = "CopilotChat - Toggle PopUp" },
+			{ "<leader>am", "<cmd>CodeCompanionModels<cr>", desc = "CopilotChat - Select Models" },
+			{ "<leader>aa", "<cmd>CodeCompanionAgents<cr>", desc = "CopilotChat - Select Agents" },
+			-- { "<leader>ae", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
 		},
 	},
 }
